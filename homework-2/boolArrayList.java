5f3b22ea36ed75cfe33c2ff639668f9c7ce19ca9
Brennan <4brothersnosisters@gmail.com>
2016-02-06 19:58:10 -0500

    started homework 2, wrote boolArrayList except for edge cases/error handling

---
+++ homework-2/boolArrayList.java
@@ -0,0 +1,50 @@
+//TODO: need to check for edge cases, ie array OOB exception
+//TODO: ask what negateAll() is supposed to return, assignment unspecific
+//TODO: exception handling? am i doing it right?  should it also return?  what?
+public class boolArrayList{
+  bool[] list;
+  int listsize;
+
+  //initializes the list array and specifies its max size
+  public boolArrayList(int maxsize){
+    list = new bool[maxsize];
+    listsize = 0;
+  }
+  //return the size of the list
+  public int size(){
+    return listsize;
+  }
+  //insert an element before the ith element or at the end if DNE
+  public void insert(int i, boolean value){
+    if(listsize < i+1)
+      list[listsize] = value;
+    else{
+      for(int j=listsize-1;j>i;j--)
+        list[j] = list[j-1];
+      list[i] = value;
+    }
+    listsize++;
+  }
+  //remove element i, or do nothing if DNE
+  public void remove(int i){
+    if!(listsize < i+1){
+      for(int j=i;j<listsize-1;j++)
+        list[j] = list[j+1];
+      listsize--;
+    }
+  }
+  //return the ith element or raise exception if DNE
+  boolean lookup(int i){
+    if(i<listsize)
+      return list[i];
+    else
+      throw new InvalidListElementException();
+  }
+  //flip all elements to their opposite, raise exception if empty list
+  boolean negateAll(){
+    if(listsize == 0)
+      throw new EmptyListException();
+    for(int i=0;i<listsize;i++)
+      list[i] = !list[i];
+  }
+}
